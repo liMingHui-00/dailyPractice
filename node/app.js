5d4de9670593a2930e0c6f380d59518a0aaca365
liminghui <3209498857@qq.com>
2023-12-23 15:14:29 +0800

    前后端分离

---
+++ node/app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+app.get("/stu", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.send({
+    status: "ok",
+    data: {
+      name: "孙悟空",
+      age: 55,
+    },
+  })
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  app
+</template>
+<script setup>
+import axios from 'axios'
+import { onMounted } from 'vue'
+onMounted(async () => {
+  const data = await axios('http://localhost:3000/stu')
+  console.log(data)
+})
+
+</script>
+
