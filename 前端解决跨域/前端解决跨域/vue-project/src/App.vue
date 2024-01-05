faa447bcd2835d5c10733790590cbc579be0e544
liMingHui-00 <102937164+liMingHui-00@users.noreply.github.com>
2024-01-05 20:16:39 +0800

    Add files via upload

---
+++ 前端解决跨域/前端解决跨域/vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    55555
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import axios from 'axios'
+onMounted(async () => {
+  const res = await axios.get('/api/stu')
+  console.log(res)
+})
+</script>
+
